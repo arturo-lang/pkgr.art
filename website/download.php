93d2af845a266a66b7a7681fd0cc69455bb970eb
drkameleon <yaniszaf@gmail.com>
2023-11-14 12:24:36 +0100

    minor edit

--- website/download.php
+++ website/download.php
@@ -2,15 +2,18 @@
 
 $pkg = $_GET["pkg"];
 $ver = $_GET["version"];
+$mgk = $_GET["mgk"];
 
-$filename = "../../" . $pkg . "-" . $ver . ".cnt";
+if (($pkg != "") && ($ver != "") && ($mgk == "18966")) {
+    $filename = "../../" . $pkg . "-" . $ver . ".cnt";
 
-$cnt = 0;
-if (file_exists($filename)){
-    $cnt = intval(file_get_contents($filename));
+    $cnt = 0;
+    if (file_exists($filename)){
+        $cnt = intval(file_get_contents($filename));
+    }
+    $cnt = $cnt + 1;
+
+    file_put_contents($cnt, $filename);
 }
-$cnt = $cnt + 1;
-
-file_put_contents($cnt, $filename);
 
 ?>
