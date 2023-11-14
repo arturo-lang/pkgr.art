71d55dc9743d191d87d97680951ef038e2356c8f
drkameleon <yaniszaf@gmail.com>
2023-11-14 13:09:40 +0100

    minor (security) edit

--- website/download.php
+++ website/download.php
@@ -5,23 +5,25 @@ $ver = $_GET["ver"];
 $mgk = $_GET["mgk"];
 
 if (($pkg != "") && ($ver != "") && ($mgk == "18966")) {
-    $filename = "/var/www/pkg_stats/" . $pkg . "-" . $ver . ".cnt";
-    $cnt = 0;
-    if (file_exists($filename)){
-        $cnt = intval(file_get_contents($filename));
-    }
-    else {
-        touch($filename);
-        chmod($filename, 0777);
-    }
-    $cnt = $cnt + 1;
+    if ((is_dir("/var/www/pkgr.art/public/" . $pkg)) && 
+        (is_dir("/var/www/pkgr.art/public/" . $pkg . "/" . $ver))) {
+        $filename = "/var/www/pkg_stats/" . $pkg . "-" . $ver . ".cnt";
+        $cnt = 0;
+        if (file_exists($filename)){
+            $cnt = intval(file_get_contents($filename));
+        }
+        else {
+            touch($filename);
+            chmod($filename, 0777);
+        }
+        $cnt = $cnt + 1;
 
-    $openFile = fopen($filename, "w+") or die("Can't open file");
-    fwrite($openFile, strval($cnt));
-    fclose($openFile);
-    fclose($myfile); 
+        $openFile = fopen($filename, "w+") or die("Can't open file");
+        fwrite($openFile, strval($cnt));
+        fclose($openFile);
 
-    echo $cnt;  
+        echo $cnt;  
+    }
 }
 
 ?>
