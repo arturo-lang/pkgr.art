dc69320600119f35f22745173f24d1127a88c346
drkameleon <yaniszaf@gmail.com>
2024-04-18 10:58:11 +0000

    CI Package Deployment

--- public/script.js
+++ public/script.js
@@ -337,6 +337,28 @@ function ajaxPost(url,action,data) {
     xmlhttp.send(JSON.stringify(data));
 }
 
+function humanFileSize(bytes, si=false, dp=1) {
+	const thresh = si ? 1000 : 1024;
+  
+	if (Math.abs(bytes) < thresh) {
+	  return bytes + ' B';
+	}
+  
+	const units = si 
+	  ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+	  : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+	let u = -1;
+	const r = 10**dp;
+  
+	do {
+	  bytes /= thresh;
+	  ++u;
+	} while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  
+  
+	return bytes.toFixed(dp) + ' ' + units[u];
+  }
+
 $(document).ready(function(){
     $.protip();
 
@@ -345,6 +367,11 @@ $(document).ready(function(){
         document.execCommand('copy');
         $(".codecopied-message").removeClass("is-hidden");
     });
+
+	$(".repo-size-field").each(function(idx) {
+		console.log();
+		$(this).text("("+humanFileSize(parseInt($(this).text().replace("(","").replace(")","")))+")");
+	});
 });
 
 function showSearch(){
